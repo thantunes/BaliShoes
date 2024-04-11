6432dc15b6d080b466f452efa7b73ac6cf917661
Guilherme <guilherme@evision.com.br>
2024-04-11 12:08:51 -0300

    vitrine

--- react/components/ProductVariations/index.jsx
+++ react/components/ProductVariations/index.jsx
@@ -1,111 +1,123 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { useCssHandles } from "vtex.css-handles";
 import { useOrderItems } from "vtex.order-items/OrderItems";
 import "./index.css";
 // import colors from './colors.css'
-import './colors.global.css'
+import "./colors.global.css";
 const CSS_HANDLES = [
-    "BaliSKUSelectorContainer",
-    "BaliSKUSelectorBoxContainer",
-    "BaliSKUSelectorMainButton",
-    "BaliSKUSelectorContainerMainButton",
-    "BaliSKUSelectorContainerSubButton",
-    "BaliSKUSelectorSubButton",
-    "buyButton",
-    'Selected'
-]
+  "BaliSKUSelectorContainer",
+  "BaliSKUSelectorBoxContainer",
+  "BaliSKUSelectorMainButton",
+  "BaliSKUSelectorContainerMainButton",
+  "BaliSKUSelectorContainerSubButton",
+  "BaliSKUSelectorSubButton",
+  "buyButton",
+  "Selected",
+];
 export default function Variations(props) {
-    const [Selected, setSelected] = useState({})
-    const [Cart, setCart] = useState({})
-    const [Color, setColor] = useState(false)
-    const { addItems } = useOrderItems();
-    const handlesUser = useCssHandles(CSS_HANDLES);
-    const hc = handlesUser.handles; // handle classes
-    var item = {}
+  const [Selected, setSelected] = useState({});
+  const [Cart, setCart] = useState({});
+  const [Color, setColor] = useState(false);
+  const { addItems } = useOrderItems();
+  const handlesUser = useCssHandles(CSS_HANDLES);
+  const hc = handlesUser.handles; // handle classes
+  var item = {};
 
-    if (props.item !== undefined) {
-        item = props.item
-    }
-    console.log(
-        {
-            id: 'teste',
-            item,
-            name: item.productName
-        }
-    )
-    var myVari = []
-    var Mytemp = []
-    item.items.map((e, i) => {
-        e.sellers?.[0].commertialOffer.AvailableQuantity > 0 ? myVari.push(
-            {
-                __COLOR: e.Cor?.[0],
-                __SIZE: e["TAMANHO"]?.[0],
-                _IMG_SRC: e.images?.[0].imageUrl,
-                __id: e.itemId,
-                __SellerId: e.sellers?.[0].sellerId
-            }
-        ) : null
-        Mytemp.push(e.Cor?.[0])
-    })
-
-    var Finish = [...new Set(Mytemp)]
-    var SeparateColors = []
-    Finish.map((el) => {
-        SeparateColors.push({
-            Name: el,
-            img: myVari.filter(e => e.__COLOR == el)?.[0]?._IMG_SRC,
-            items: myVari.filter(e => e.__COLOR == el),
-            ProdID: myVari.filter(e => e.__COLOR == el)?.[0]?.__id
+  if (props.item !== undefined) {
+    item = props.item;
+  }
+  console.log({
+    id: "teste",
+    item,
+    name: item.productName,
+  });
+  var myVari = [];
+  var Mytemp = [];
+  item.items.map((e, i) => {
+    e.sellers?.[0].commertialOffer.AvailableQuantity > 0
+      ? myVari.push({
+          __COLOR: e.Cor?.[0],
+          __SIZE: e["TAMANHO"]?.[0],
+          _IMG_SRC: e.images?.[0].imageUrl,
+          __id: e.itemId,
+          __SellerId: e.sellers?.[0].sellerId,
         })
-    })
-    function normalizeText(text) {
-        return text?.toLowerCase()?.replace(/\s+/g, '-').replace(/\//g, '-');
-    }
-    return (
-        <div className={hc["BaliSKUSelectorContainer"]}>
-            <div className={hc["BaliSKUSelectorContainerMainButton"]}>
-                {SeparateColors.map((e, i) => e.items.length > 0 && i < 3 ? (
-                    <div style={{margin:'0px 5px'}}>
-                        <div className={hc["BaliSKUSelectorBoxContainer"]}>
-                            <button className={`${hc["BaliSKUSelectorMainButton"]} PS-Color PS-Color-${normalizeText(e.Name)} ${Selected.ProdID == e.ProdID ? 'PS-color-Active': ''}`} onClick={(event) => {
-                                // console.log(event.target)
-                                setSelected(e)
-                                setColor(true)
-                                console.log({ e })
-                                setCart({ id: e.items[0].__id, seller: e.items[0].__SellerId, quantity: 1 })
-                            }}>
-                                {/* <img width={50} height={70} src={e.img} alt="Color Image" /> */}
-                                <p style={{opacity:0}}>{e.Name}</p>
-                            </button>
-                        </div>
-                        <div className={hc["BaliSKUSelectorContainerSubButton"]} id={e.ProdID} style={{ display: Selected.ProdID == e.ProdID ? 'flex' : 'none' }}>
-                            {/* {
-                                e.items.map(f => (
-                                    <button CartID={f.__id} className={`${Cart.id == f.__id ? hc['Selected'] : ''} ${hc["BaliSKUSelectorSubButton"]}`} onClick={(event2) => {
-                                        // console.log(event2.target.getAttribute('CartID'))
-                                        setCart({ id: event2.target.getAttribute('CartID'), seller: f.__SellerId, quantity: 1 })
-                                        console.log(f.__SellerId, f.__id)
-                                    }} >
-                                        <span CartID={f.__id} style={{ textTransform: 'uppercase' }} >
-                                            {f.__SIZE}
-                                        </span>
-                                    </button>
-                                ))
-                            } */}
-                        </div>
-                    </div>
-                ) : null)}
+      : null;
+    Mytemp.push(e.Cor?.[0]);
+  });
+
+  var Finish = [...new Set(Mytemp)];
+  var SeparateColors = [];
+  Finish.map((el) => {
+    SeparateColors.push({
+      Name: el,
+      img: myVari.filter((e) => e.__COLOR == el)?.[0]?._IMG_SRC,
+      items: myVari.filter((e) => e.__COLOR == el),
+      ProdID: myVari.filter((e) => e.__COLOR == el)?.[0]?.__id,
+    });
+  });
+  function normalizeText(text) {
+    return text?.toLowerCase()?.replace(/\s+/g, "-").replace(/\//g, "-");
+  }
+  return (
+    <div className={hc["BaliSKUSelectorContainer"]}>
+      <div className={hc["BaliSKUSelectorContainerMainButton"]}>
+        {SeparateColors.map((e, i) =>
+          e.items.length > 0 && i < 3 ? (
+            <div style={{ margin: "0px 5px" }}>
+              <div className={hc["BaliSKUSelectorBoxContainer"]}>
+                <button
+                  className={`${
+                    hc["BaliSKUSelectorMainButton"]
+                  } PS-Color PS-Color-${normalizeText(e.Name)} ${
+                    Selected.ProdID == e.ProdID ? "PS-color-Active" : ""
+                  }`}
+                  onClick={(event) => {
+                    // console.log(event.target)
+                    setSelected(e);
+                    setColor(true);
+                    console.log({ e });
+                    setCart({
+                      id: e.items[0].__id,
+                      seller: e.items[0].__SellerId,
+                      quantity: 1,
+                    });
+                    console.log(
+                      document.querySelectorAll(`img[id="${props.imageref}"]`)[1],
+                      "ref",
+                      (document.querySelectorAll(`img[id="${props.imageref}"]`)[1].src =
+                        e.img),
+                      props.imageref,
+                      e.img
+                    );
+                  }}
+                >
+                  {/* <img width={50} height={70} src={e.img} alt="Color Image" /> */}
+                  <p style={{ opacity: 0 }}>{e.Name}</p>
+                </button>
+              </div>
+              <div
+                className={hc["BaliSKUSelectorContainerSubButton"]}
+                id={e.ProdID}
+                style={{
+                  display: Selected.ProdID == e.ProdID ? "flex" : "none",
+                }}
+              ></div>
             </div>
-            <button className={hc["buyButton"]} onClick={() => {
-                addItems([Cart])
-                setCart([{}])
-                setColor(false)
-                setSelected({})
-            }}>
-                {
-                    !Color ? 'Selecione uma variação' : 'EU QUERO'
-                }
-            </button>
-        </div>
-    )
+          ) : null
+        )}
+      </div>
+      <button
+        className={hc["buyButton"]}
+        onClick={() => {
+          addItems([Cart]);
+          setCart([{}]);
+          setColor(false);
+          setSelected({});
+        }}
+      >
+        {!Color ? "Selecione uma variação" : "EU QUERO"}
+      </button>
+    </div>
+  );
 }
--- react/components/StorefrontCustom/index.jsx
+++ react/components/StorefrontCustom/index.jsx
@@ -3,229 +3,221 @@ import axios from "axios";
 import React, { useEffect, useRef, useState } from "react";
 import { useCssHandles } from "vtex.css-handles";
 import { SliderLayout, SliderLayoutGroup } from "vtex.slider-layout";
-
 import StorefrontFlagDiscount from "../StorefrontFlagDiscount";
-
 import "../../css/styles.css";
 import Variations from "../ProductVariations";
 import "./index.css";
 
 const CSS_HANDLES = [
-    "BaliPSMain",
-    "BaliPSBlockTitle",
-    "BaliPSTitle",
-    "BaliPSBlockTop",
-    "BaliPSBlockTopLink",
-    "BaliPSBlockImage",
-    "BaliPSFirstImage",
-    "BaliPSSecondImage",
-    "BaliPSBlockSize",
-    "BaliPSBlockInfo",
-    "BaliPSBlockName",
-    "BaliPSName",
-    "BaliPSBlockPrice",
-    "BaliPSBlockPriceListToBest",
-    "BaliPSListPrice",
-    "BaliPSBestPrice",
-    "BaliPSInstallmentPrice",
-    "BaliPSBlockBottomLink",
-    "BaliPSPrimarySliderContainer"
+  "BaliPSMain",
+  "BaliPSBlockTitle",
+  "BaliPSTitle",
+  "BaliPSBlockTop",
+  "BaliPSBlockTopLink",
+  "BaliPSBlockImage",
+  "BaliPSFirstImage",
+  "BaliPSSecondImage",
+  "BaliPSBlockSize",
+  "BaliPSBlockInfo",
+  "BaliPSBlockName",
+  "BaliPSName",
+  "BaliPSBlockPrice",
+  "BaliPSBlockPriceListToBest",
+  "BaliPSListPrice",
+  "BaliPSBestPrice",
+  "BaliPSInstallmentPrice",
+  "BaliPSBlockBottomLink",
+  "BaliPSPrimarySliderContainer",
 ];
 
 const StorefrontCustom = (props) => {
-    // Instance the generator of class
-    const handlesUser = useCssHandles(CSS_HANDLES);
-    const hc = handlesUser.handles; // handle classes
-    const CollectionId = props.message
-    const CollectionLink = props.collectionlink
-    const CollectionTitle = props.collectiontitle
-    console.log(CollectionLink, CollectionId)
-    const [productList, setProductList] = useState(null);
-    let sliderRef1 = useRef(null);
-    let sliderRef2 = useRef(null);
-    const getProductList = async () => {
-        await axios({
-            method: "get",
-            url: `/api/catalog_system/pub/products/search?fq=productClusterIds:${CollectionId}`,
-        }).then((resp) => {
-            if (resp.data && resp.data.length) {
-                setProductList(resp.data);
-            }
-        });
-    };
-    const itemsPerPage = {
-        desktop: 4,
-        tablet: 3,
-        phone: 1,
-    };
+  // Instance the generator of class
+  const handlesUser = useCssHandles(CSS_HANDLES);
+  const hc = handlesUser.handles; // handle classes
+  const CollectionId = props.message;
+  const CollectionLink = props.collectionlink;
+  const CollectionTitle = props.collectiontitle;
+  console.log(CollectionLink, CollectionId);
+  const [productList, setProductList] = useState(null);
+  let sliderRef1 = useRef(null);
+  let sliderRef2 = useRef(null);
+  const refImage = useRef(null);
+  const [uniqid, setuniqid] = useState(Math.random().toString(16).slice(2));
+  const getProductList = async () => {
+    await axios({
+      method: "get",
+      url: `/api/catalog_system/pub/products/search?fq=productClusterIds:${CollectionId}`,
+    }).then((resp) => {
+      if (resp.data && resp.data.length) {
+        setProductList(resp.data);
+      }
+    });
+  };
+  const itemsPerPage = {
+    desktop: 4,
+    tablet: 3,
+    phone: 1,
+  };
 
-    const findSmallerInstallment = (list) => {
-        let res = list.reduce((item, itemNext) =>
-            item.Value < itemNext.Value ? item : itemNext
-        );
-        return res;
-    };
+  const findSmallerInstallment = (list) => {
+    let res = list.reduce((item, itemNext) =>
+      item.Value < itemNext.Value ? item : itemNext
+    );
+    return res;
+  };
 
-    const RenderItem = (props) => {
-        let item = props.data.dataItem;
-        let defaultItem = item.items[0].sellers[0];
-        let style = props.style ? props.style : {};
-        let link = `/${item.linkText}/p`;
-        let listPrice = defaultItem.commertialOffer.ListPrice;
-        let bestPrice = defaultItem.commertialOffer.Price;
+  const RenderItem = (props) => {
+    let item = props.data.dataItem;
+    let defaultItem = item.items[0].sellers[0];
+    let style = props.style ? props.style : {};
+    let link = `/${item.linkText}/p`;
+    let listPrice = defaultItem.commertialOffer.ListPrice;
+    let bestPrice = defaultItem.commertialOffer.Price;
 
-        listPrice = listPrice.toLocaleString("pt-BR", {
-            style: "currency",
-            currency: "BRL",
-        });
-        bestPrice = bestPrice.toLocaleString("pt-BR", {
-            style: "currency",
-            currency: "BRL",
-        });
+    listPrice = listPrice.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+    bestPrice = bestPrice.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
 
-        let installmentText;
-        if (
-            defaultItem.commertialOffer.Installments &&
-            defaultItem.commertialOffer.Installments.length
-        ) {
-            let smallInstallment = findSmallerInstallment(
-                defaultItem.commertialOffer.Installments
-            );
-            let totalInstallments = smallInstallment.NumberOfInstallments;
-            let valueInstallment = smallInstallment.Value.toLocaleString(
-                "pt-BR",
-                {
-                    style: "currency",
-                    currency: "BRL",
-                }
-            );
-            installmentText = `ou ${totalInstallments}x de ${valueInstallment} sem juros`;
-        } else {
-            installmentText = "";
-        }
+    let installmentText;
+    if (
+      defaultItem.commertialOffer.Installments &&
+      defaultItem.commertialOffer.Installments.length
+    ) {
+      let smallInstallment = findSmallerInstallment(
+        defaultItem.commertialOffer.Installments
+      );
+      let totalInstallments = smallInstallment.NumberOfInstallments;
+      let valueInstallment = smallInstallment.Value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      });
+      installmentText = `ou ${totalInstallments}x de ${valueInstallment} sem juros`;
+    } else {
+      installmentText = "";
+    }
 
-        return (
-            <div className={hc.BaliPSMain} style={style}>
-                <div className={hc.BaliPSBlockTop}>
-                    <a href={link} className={hc.BaliPSBlockTopLink}>
-                        <div className={hc.BaliPSBlockName}>
-                            <span className={hc.BaliPSName}>
-                                {item.productName}
-                            </span>
-                        </div>
-                        <StorefrontFlagDiscount product={item}/>
+    return (
+      <div className={hc.BaliPSMain} style={style}>
+        <div className={hc.BaliPSBlockTop}>
+          <a href={link} className={hc.BaliPSBlockTopLink}>
+            <div className={hc.BaliPSBlockName}>
+              <span className={hc.BaliPSName}>{item.productName}</span>
+            </div>
+            <StorefrontFlagDiscount product={item} />
 
-                        <div className={hc.BaliPSBlockImage}>
-                            <img
-                                src={item.items[0].images[0].imageUrl}
-                                className={hc.BaliPSFirstImage}
-                            />
-                            {item.items[0].images[1] &&
-                                item.items[0].images[1].imageUrl && (
-                                    <img
-                                        src={item.items[0].images[1].imageUrl}
-                                        className={hc.BaliPSSecondImage}
-                                    />
-                                )}
-                        </div>
+            <div className={hc.BaliPSBlockImage}>
+              <img
+                src={item.items[0].images[0].imageUrl}
+                
+                className={hc.BaliPSFirstImage}
 
-                    </a>
-                </div>
-                <div className={hc.BaliPSBlockSize}>
-                    <div className={hc.BaliPSBlockInfo}>
-                        <a href={link} className={hc.BaliPSBlockBottomLink}>
-                            <div className={hc.BaliPSBlockPrice}>
-                                <span className={hc.BaliPSBlockPriceListToBest}>
-                                    {/* <del className={hc.BaliPSListPrice}>{listPrice}</del> */}
-                                    <span className={hc.BaliPSBestPrice}>
-                                        {bestPrice}
-                                    </span>
-                                </span>
-                                <span className={hc.BaliPSInstallmentPrice}>
-                                    {installmentText}
-                                </span>
-                            </div>
-                        </a>
-                    </div>
-                    {/* <StorefrontCustomSizeVariation
+                id={item.items[0].images[0].imageId}
+              />
+              {item.items[0].images[1] && item.items[0].images[1].imageUrl && (
+                <img
+                  src={item.items[0].images[1].imageUrl}
+                  className={hc.BaliPSSecondImage}
+                />
+              )}
+            </div>
+          </a>
+        </div>
+        <div className={hc.BaliPSBlockSize}>
+          <div className={hc.BaliPSBlockInfo}>
+            <a href={link} className={hc.BaliPSBlockBottomLink}>
+              <div className={hc.BaliPSBlockPrice}>
+                <span className={hc.BaliPSBlockPriceListToBest}>
+                  {/* <del className={hc.BaliPSListPrice}>{listPrice}</del> */}
+                  <span className={hc.BaliPSBestPrice}>{bestPrice}</span>
+                </span>
+                <span className={hc.BaliPSInstallmentPrice}>
+                  {installmentText}
+                </span>
+              </div>
+            </a>
+          </div>
+          {/* <StorefrontCustomSizeVariation
                         dataItem={{ item }}
                     /> */}
-                    <div>
-                        <Variations item={item} />
-                    </div>
-                </div>
+          <div>
+            <Variations imageref={item.items[0].images[0].imageId} item={item} />
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  useEffect(() => {
+    getProductList();
+  }, []);
+  console.log("load", productList);
+
+  const RenderShelf = () => {
+    if (!productList) {
+      return null;
+    }
+
+    return (
+      <div className={hc.BaliPSMain}>
+        <div className={hc.BaliPSBlockTitle}>
+          <h2 className={hc.BaliPSTitle}>{CollectionTitle}</h2>
+        </div>
+
+        <div>
+          <SliderLayoutGroup>
+            <div className={hc.BaliPSPrimarySliderContainer}>
+              <a href={CollectionLink}> VER MAIS</a>
+              <SliderLayout
+                itemsPerPage={itemsPerPage}
+                usePagination={true}
+                asNavFor={sliderRef1}
+                ref={(slider) => (sliderRef2 = slider)}
+                showPaginationDots={"never"}
+              >
+                {productList &&
+                  productList.map((item, index) => (
+                    <RenderItem
+                      key={index}
+                      data={{
+                        dataItem: item,
+                        dataIndex: index,
+                      }}
+                      style={{ display: "none" }}
+                    />
+                  ))}
+              </SliderLayout>
             </div>
-        );
-    };
+            <SliderLayout
+              showPaginationDots={"never"}
+              itemsPerPage={itemsPerPage}
+              usePagination={true}
+              asNavFor={sliderRef1}
+              ref={(slider) => (sliderRef2 = slider)}
+              showNavigationArrows={"never"}
+            >
+              {productList &&
+                productList.map((item, index) => (
+                  <RenderItem
+                    key={index}
+                    data={{
+                      dataItem: item,
+                      dataIndex: index,
+                    }}
+                  />
+                ))}
+            </SliderLayout>
+          </SliderLayoutGroup>
+        </div>
+      </div>
+    );
+  };
 
-    useEffect(() => {
-        getProductList();
-    }, []);
-    console.log('load', productList)
-
-    const RenderShelf = () => {
-        if (!productList) {
-            return null;
-        }
-
-        return (
-            <div className={hc.BaliPSMain}>
-                <div className={hc.BaliPSBlockTitle}>
-                    <h2 className={hc.BaliPSTitle}>{CollectionTitle}</h2>
-                </div>
-
-                <div>
-                    <SliderLayoutGroup>
-                        <div className={hc.BaliPSPrimarySliderContainer}>
-                            <a href={CollectionLink}> VER MAIS</a>
-                            <SliderLayout
-                                itemsPerPage={itemsPerPage}
-                                usePagination={true}
-                                asNavFor={sliderRef1}
-                                ref={slider => (sliderRef2 = slider)}
-                                showPaginationDots={'never'}
-                            >
-                                {productList &&
-                                    productList.map((item, index) => (
-                                        <RenderItem
-                                            key={index}
-                                            data={{
-                                                dataItem: item,
-                                                dataIndex: index,
-                                            }}
-                                            style={{ display: 'none' }}
-                                        />
-                                    ))}
-                            </SliderLayout>
-                        </div>
-                        <SliderLayout
-                            showPaginationDots={'never'}
-                            itemsPerPage={itemsPerPage}
-                            usePagination={true}
-                            asNavFor={sliderRef1}
-                            ref={slider => (sliderRef2 = slider)}
-                            showNavigationArrows={"never"}
-
-                        >
-                            {productList &&
-                                productList.map((item, index) => (
-                                    <RenderItem
-                                        key={index}
-                                        data={{
-                                            dataItem: item,
-                                            dataIndex: index,
-                                        }}
-                                    />
-                                ))}
-                        </SliderLayout>
-
-                    </SliderLayoutGroup>
-                </div>
-            </div>
-        );
-    };
-
-    return <RenderShelf />;
+  return <RenderShelf />;
 };
 
 // StorefrontCustom.getSchema = () => {
