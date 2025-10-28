75323256c57484dd270ca8898ca3a848fea9091f
thantunes <71597437+thantunes@users.noreply.github.com>
2025-10-28 18:19:59 -0300

    Black Friday

--- react/components/MobileSearchBtn/index.jsx
+++ react/components/MobileSearchBtn/index.jsx
@@ -17,7 +17,8 @@ const MobileSearchBtn = () => {
 
   return (
     <div onClick={() => setOpen(!open)}>
-      <img src="/arquivos/search-icon.svg" width={21} height={21} />
+      {/* <img src="/arquivos/search-icon.svg" width={21} height={21} -- BLACK FRIDAY -- /> */}
+      <img src="https://balishoes.vtexassets.com/assets/vtex.file-manager-graphql/images/2b4daf54-2cdc-46b8-9132-787c60bd2acc___4320369dc235a5afe41b95176b570214.png" width={21} height={21} />
     </div>
   );
 };
